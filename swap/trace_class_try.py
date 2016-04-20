631b9ae1b4cefca5da2cea47a13ea196d862f70d
zwidny <649038269@qq.com>
2016-04-20 20:47:41 +0800

    update

--- swap/trace_class_try.py
+++ swap/trace_class_try.py
@@ -1,49 +1,40 @@
-import pdb
-from inspect import getsource
+# -*- coding: utf-8 -*-
 
 
 class MetaX(type):
-    def __new__(meta, *args):
+    def __new__(meta, classname, supers, classdict):
         print("__new__")
         print('meta: ', meta)
-        print('args: ', args)
-        x = type.__new__(meta, *args)
-        print(getsource(x))
+        print('classname: ', classname)
+        print('supers: ', supers)
+        print('classdict: ', classdict)
+        x = type.__new__(meta, classname, supers, classdict)
         return x
 
-    def __init__(cls, what, bases=None, dict=None):
+    def __init__(cls, classname, supers, classdict):
         print("__init__")
         print('cls: ', cls)
-        print('what: ', what)
-        print('bases: ', bases)
-        print('dict: ', dict)
-        type.__init__(cls, what, bases, dict)
-
-    def __call__(cls, *args):
-        print("__call__")
-        print('cls: ', cls)
-        print('args: ', args)
-        x = type.__call__(cls, *args)
-        print(getsource(x))
-        return x
+        print('classname: ', classname)
+        print('supers: ', supers)
+        print('classdict: ', classdict)
+        type.__init__(cls, classname, supers, classdict)
 
 
 if __name__ == '__main__':
-    print("*"*20, 'class', "*"*20)
-
+    print("*"*20, 'make class', "*"*20)
 
     class A(object):
         __metaclass__ = MetaX
 
-        def __call__(self, *args, **kwargs):
-            print("__call__")
-            return super(A, self).__call__(*args, **kwargs)
+        # def __call__(self, *args, **kwargs):
+        #     print("__call__")
+        #     return super(A, self).__call__(*args, **kwargs)
 
         def __init__(self, a, b):
             self.a = a
             self.b = b
 
-    print("*"*20, 'instance', "*"*20)
+    print("*"*20, 'make instance', "*"*20)
     A(1, 2)
 
     # SM.__call__(A)
